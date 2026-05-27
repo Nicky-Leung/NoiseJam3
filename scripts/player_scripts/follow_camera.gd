1ef4203daf75ad2ac207e0e86eee413e4266c25a
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2026-05-27 10:26:15 -0700

    change to use mouse position on screen

--- scripts/player_scripts/follow_camera.gd
+++ scripts/player_scripts/follow_camera.gd
@@ -2,9 +2,11 @@ extends Camera2D
 
 const default_zoom = 1.75
 
-@export var offset_threshold: float = 120
 @export var offset_amount: float = 70
 @export var offset_speed: float = 5
+
+const x_threshold = 150
+const y_threshold = 100
 
 func transition_zoom(amount = default_zoom, trans_time = 0.25):
 	if trans_time <= 0:
@@ -17,6 +19,16 @@ func transition_zoom(amount = default_zoom, trans_time = 0.25):
 func _physics_process(delta):
 	# camera offsetting when moving mouse towards edge of screen
 	var offset_dir
-	if global_position.distance_to(get_global_mouse_position()) < offset_threshold: offset_dir = Vector2.ZERO
-	else: offset_dir = global_position.direction_to(get_global_mouse_position())
+	if mouse_out_of_range(): offset_dir = global_position.direction_to(get_global_mouse_position())
+	else: offset_dir = Vector2.ZERO
 	offset = offset.lerp(offset_dir * offset_amount, offset_speed * delta)
+
+func mouse_out_of_range() -> bool:
+	var mouse_pos = get_viewport().get_mouse_position()
+	var viewport_size = get_viewport().get_visible_rect().size
+	return (
+		mouse_pos.x < x_threshold
+		|| mouse_pos.x > viewport_size.x - x_threshold
+		|| mouse_pos.y < y_threshold
+		|| mouse_pos.y > viewport_size.y - y_threshold
+	)
