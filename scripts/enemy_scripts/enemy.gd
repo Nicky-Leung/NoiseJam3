10d0a0fabed8d14206d3d9cca414321fc9606206
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2026-05-30 11:59:52 -0700

    add ability to take damage and die via weeping angel

--- scripts/enemy_scripts/enemy.gd
+++ scripts/enemy_scripts/enemy.gd
@@ -6,16 +6,21 @@ class_name Enemy
 @export var nav_agent: NavigationAgent2D = null
 @export var animated_sprite: AnimatedSprite2D = null
 
-# stats
+# start state
 @export var collides_with_others: bool = false # check if enemy needs to collider with other enemies
 @export var is_active: bool = false # set it to true in editor if enemy should immediately work on scene load
+@export var frames_per_nav_check = 60
+
+# stats
+@export var attack_damage: int = 10
+@export var turn_rate: float = 10
 @export var acceleration: int = 20
 @export var base_speed: int = 100
-@export var frames_per_nav_check = 60
 
 # running variables
 var nav_check_stagger = 0 # stagger so not all nav agents check in the same frame
 var in_light: bool = false
+var face_direction: Vector2 = Vector2.ZERO
 var move_direction: Vector2 = Vector2.ZERO
 var chase_target: Player = null # for chasing (set this to start chasing)
 
@@ -44,6 +49,7 @@ func can_check_nav() -> bool:
 
 func chase(delta: float) -> bool: # default chase behavior implementation subclasses can use
 	move_and_slide() # move based on previous frame values
+	turn_process(delta)
 
 	if chase_target == null || !can_check_nav(): return false
 	nav_agent.target_position = chase_target.global_position
@@ -52,3 +58,8 @@ func chase(delta: float) -> bool: # default chase behavior implementation subcla
 	if !nav_agent.is_target_reached():
 		velocity = velocity.lerp(move_direction * base_speed, acceleration * delta)
 	return nav_agent.is_target_reached()
+
+func turn_process(delta):
+	var new_direction = face_direction.lerp(move_direction, turn_rate * delta)
+	rotation = new_direction.angle()
+	face_direction = new_direction
--- scripts/enemy_scripts/weeping_angel.gd
+++ scripts/enemy_scripts/weeping_angel.gd
@@ -9,8 +9,15 @@ func _ready():
 
 func _physics_process(delta):
     if !is_active || in_light: return
+
     chase(delta)
+    try_damage_player()
     footsteps.play_steps(velocity.length() * 2, 0.1, 1.1, 1.2)
 
 func alert_visual(alerter: Node2D) -> void:
     super(alerter)
+
+func try_damage_player():
+    var collider = attack_ray.get_collider()
+    if collider is not Player: return
+    (collider as Player).attack(attack_damage, self)
