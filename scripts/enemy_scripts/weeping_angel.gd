9860fc2221457915026bbc8df74f44c156ebb433
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2026-05-30 10:41:57 -0700

    implement weeping angel chase logic

--- scripts/enemy_scripts/weeping_angel.gd
+++ scripts/enemy_scripts/weeping_angel.gd
@@ -1,7 +1,16 @@
 extends Enemy
 
+@onready var footsteps = $Footsteps
 @onready var attack_ray: RayCast2D = $AttackRay
+
+func _ready():
+    super()
+    chase_target = player
+
+func _physics_process(delta):
+    if !is_active || in_light: return
+    chase(delta)
+    footsteps.play_steps(velocity.length() * 2)
 
 func alert_visual(alerter: Node2D) -> void:
     super(alerter)
-    # implement mechanics later
