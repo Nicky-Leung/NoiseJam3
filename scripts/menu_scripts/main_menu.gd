467bc578f97d926b184f09e91c8fff423cf600ef
persephone-wong <persephone.w315@gmail.com>
2026-06-02 13:20:13 -0700

    Change Start Menu

--- scripts/menu_scripts/main_menu.gd
+++ scripts/menu_scripts/main_menu.gd
@@ -1,27 +1,46 @@
 extends Control
 
-@onready var start: Button = $Play
-@onready var options: Button = $Options
-@onready var credits: Button = $Credits
-@onready var quit: Button = $Quit
+@onready var start: TextureButton = $Start
+@onready var options: TextureButton = $Options
+@onready var credits: TextureButton = $Credits
+@onready var quit: TextureButton = $Quit
 
 func _ready():
+	z_index = 10
+	start.z_index = 10
+	options.z_index = 10
+	credits.z_index = 10
+	quit.z_index = 10
+	if start == null:
+		print("ERROR: Play button not found at path: $Play")
+		return
+	if options == null:
+		print("ERROR: Options button not found at path: $Options")
+		return
+	if credits == null:
+		print("ERROR: Credits button not found at path: $Credits")
+		return
+	
 	start.pressed.connect(handle_start)
 	options.pressed.connect(handle_options)
 	credits.pressed.connect(handle_credits)
 
-	if OS.has_feature("web"): # quitting in browser is equivalent to crashing the game
-		quit.visible = false
+	if OS.has_feature("web"):
+		if quit:
+			quit.visible = false
 	else:
-		quit.pressed.connect(handle_quit)
+		if quit == null:
+			print("ERROR: Quit button not found at path: $Quit")
+		else:
+			quit.pressed.connect(handle_quit)
 
-func handle_start(): # probably need to do some fade-to-game type thing instead of abrupt cut
+func handle_start():
 	SCENE_MANAGER.change_scene(SceneManager.Scenes.IN_GAME)
 
-func handle_options(): # implement options menu
+func handle_options():
 	SCENE_MANAGER.open_sub_menu(SceneManager.SubMenus.OPTIONS)
 
-func handle_credits(): # implement at the end when all the assets are sorted out
+func handle_credits():
 	print("credits opened")
 
 func handle_quit():
