41b5bdd750132d43046c3c862e302dd3d0bd29f7
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2026-06-05 13:28:09 -0700

    add scouting the area behavior after reaching sound position

--- scripts/enemy_scripts/mole_rat.gd
+++ scripts/enemy_scripts/mole_rat.gd
@@ -6,49 +6,82 @@ extends Enemy
 # Components
 @onready var vision: EnemyVision = $VisionCone
 @onready var reset_timer = $ResetTimer
+@onready var idle_timer = $IdleTimer
 @onready var footsteps = $Footsteps
 
 # Runtime variables
 var scout_position: Vector2 = Vector2.ZERO
 var last_patrol_position: Vector2 = Vector2.ZERO
+var idle_turn_angle: float = 0
+var idle_move_frames: int = 0
+var doing_idle_movement: bool = false
 
 func _ready():
-    super()
-    ai_state = State.PATROL
-    reset_timer.wait_time = attention_time
-    reset_timer.timeout.connect(on_timeout)
+	super()
+	ai_state = State.PATROL
+	reset_timer.wait_time = attention_time
+	reset_timer.timeout.connect(on_reset_timeout)
+	idle_timer.timeout.connect(on_idle_timeout)
 
 func _process(_delta):
-    pass
+	pass
 
 func _physics_process(delta):
-    if !is_active: return
+	if !is_active: return
 
-    if ai_state == State.PATROL:
-        patrol(delta)
-        last_patrol_position = global_position
-        rotation = move_toward(rotation, 0, delta * turn_rate)
-        return
+	if ai_state == State.PATROL:
+		patrol(delta)
+		last_patrol_position = global_position
+		rotation = move_toward(rotation, 0, delta * turn_rate)
 
-    elif ai_state == State.SCOUT:
-        nav_agent.target_desired_distance = 50
-        var reached = move_to(delta, scout_position)
-        if reached:
-            ai_state = State.IDLE
-            reset_timer.start()
+	elif ai_state == State.IDLE: # create random movement to appear like scouting nearby
+		if !doing_idle_movement:
+			if randi() % 2 == 0: idle_turn_angle = randf() * PI - PI / 4
+			else: idle_move_frames = randi() % 40 + 25
+			doing_idle_movement = true
+		else:
+			if idle_move_frames > 0: move_forwards()
+			else: look_at_idle_angle(delta)
 
-    elif ai_state == State.RETURN:
-        nav_agent.target_desired_distance = 1
-        var reached = move_to(delta, last_patrol_position)
-        if reached: ai_state = State.PATROL
+	elif ai_state == State.SCOUT:
+		nav_agent.target_desired_distance = 50
+		var reached = move_to(delta, scout_position)
+		if reached:
+			ai_state = State.IDLE
+			reset_timer.start()
+
+	elif ai_state == State.RETURN:
+		nav_agent.target_desired_distance = 1
+		var reached = move_to(delta, last_patrol_position)
+		if reached:
+			ai_state = State.PATROL
+			idle_timer.start()
 
 func alert_sound(alerter: Node2D) -> void:
-    ai_state = State.SCOUT
-    reset_timer.stop()
-    scout_position = alerter.global_position
+	ai_state = State.SCOUT
+	scout_position = alerter.global_position + alerter.global_position.direction_to(global_position) * 50 # offset from player by tiny bit
+	reset_timer.stop()
+	idle_timer.stop()
+
+func move_forwards():
+	velocity = Vector2.from_angle(global_rotation) * randf() * base_speed
+	move_and_slide()
+	idle_move_frames -= 1
+	doing_idle_movement = idle_move_frames > 0
+
+func look_at_idle_angle(delta: float):
+	var new_direction = face_direction.lerp(Vector2.from_angle(idle_turn_angle), turn_rate * delta)
+	global_rotation = new_direction.angle()
+	face_direction = new_direction
+	doing_idle_movement = abs(global_rotation - idle_turn_angle) > 0.01
 
 func on_in_view(player: Player):
-    pass
+	pass
 
-func on_timeout():
-    ai_state = State.RETURN
+func on_idle_timeout():
+	ai_state = State.IDLE
+	idle_timer.wait_time = randi() % 11 + 10
+	reset_timer.start()
+
+func on_reset_timeout():
+	ai_state = State.RETURN
