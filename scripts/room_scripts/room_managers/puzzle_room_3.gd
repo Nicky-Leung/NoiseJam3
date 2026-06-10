8cadb72ee273f9740370da5cc335d9dd33fbfe8e
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2026-06-10 08:50:16 -0700

    add cooldown and sfx to gate

--- scripts/room_scripts/room_managers/puzzle_room_3.gd
+++ scripts/room_scripts/room_managers/puzzle_room_3.gd
@@ -1,13 +1,20 @@
 extends Node2D
 
-@export var gate_tiles: Array[Vector2i] = []
+@export var gate_layer: TileMapLayer = null
 
+@onready var wall_layer: TileMapLayer = $Layout/Walls
 @onready var room_listener: RoomListener = $Layout/RoomListener
 @onready var cage_area: Area2D = $Layout/DungeonArea
 @onready var gate_switch: Interactable = $Computer/Interactable
+
+@onready var gate_cd: Timer = $Timers/GateCD
+
 @onready var alarm: AudioStreamPlayer2D = $Sounds/Alarm
+@onready var gate_noise: AudioStreamPlayer2D = $Sounds/Gate
+@onready var gate_error: AudioStreamPlayer2D = $Sounds/GateError
 
 var enemy_in_cage: bool = true
+var cage_locked: bool = true
 
 func _ready():
 	cage_area.body_entered.connect(func(body): _on_cage_change(body, true))
@@ -24,6 +31,13 @@ func _on_cage_change(body: Node2D, entered: bool):
 	if body is not Titania: return
 	enemy_in_cage = entered
 
-func _on_gate_switch_pressed():
+func _on_gate_switch_pressed(_player: Player):
+	if gate_cd.time_left > 0:
+		HELPERS.play_audio(gate_error, 1.1, 1.2, -5)
+		return
+
+	gate_cd.start()
+	cage_locked = !cage_locked
+	gate_layer.enabled = cage_locked
 	HELPERS.play_audio(alarm)
-	pass # change tiles at gate tiles to invisible and de-activate collision on them
+	HELPERS.play_audio(gate_noise, 0.9, 1.1, -5)
