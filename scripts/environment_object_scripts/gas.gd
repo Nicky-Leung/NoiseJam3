ad82cc119a6e3420e47f7ea8399c080761daf321
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2026-06-10 12:25:55 -0700

    add gas sound to room

--- scripts/environment_object_scripts/gas.gd
+++ scripts/environment_object_scripts/gas.gd
@@ -1,6 +1,7 @@
 extends Node
 class_name Gas
 
+@export var gas_sound: AudioStreamPlayer2D = null
 @export var gas_shader: ColorRect = null
 @export var damage_timer: Timer = null
 @export var time_before_damage_starts: float = 7
@@ -25,16 +26,19 @@ func start_gas():
 	if is_running: return # don't do anything if already running
 	is_running = true
 	gas_released_time = 0
+	HELPERS.play_audio(gas_sound, 0, 0, -10)
 
 func stop_gas():
 	is_running = false
 	damage_timer.stop()
 
 	var tween = create_tween()
-	tween.tween_method(
+	tween.tween_property(gas_sound, "volume_db", -60, 2)
+	tween.parallel().tween_method(
 		func(value): gas_shader.material.set_shader_parameter("density", value),
 		gas_shader.material.get_shader_parameter("density"), 0, 2
 	)
+	tween.tween_callback(func(): gas_sound.stop())
 	tween.play()
 
 func _damage_player():
