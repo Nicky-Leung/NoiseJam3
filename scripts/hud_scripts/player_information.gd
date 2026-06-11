16e51a25fc3012cc70c3141005a4fcbb6de4933e
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2026-06-11 05:11:19 -0700

    connect battery charge to UI

--- scripts/hud_scripts/player_information.gd
+++ scripts/hud_scripts/player_information.gd
@@ -7,11 +7,13 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	health_bar.max_value = player.max_health
-	pass # Replace with function body.
-
+	call_deferred("set_max")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	health_bar.value = player.health
-	# battery_bar.value = player.battery
+	battery_bar.value = player.flashlight.battery_time
+
+func set_max():
+	health_bar.max_value = player.max_health
+	battery_bar.max_value = player.flashlight.max_battery_seconds
