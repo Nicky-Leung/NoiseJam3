e2c0096cbfa5df9ef1e4e1b71377f8712f66886e
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2026-06-12 10:33:23 -0700

    prepare room for combining with main level

--- scripts/environment_object_scripts/censored_sin.gd
+++ scripts/environment_object_scripts/censored_sin.gd
@@ -5,12 +5,21 @@ extends Node2D
 
 var is_censored: bool = true
 
-# need to see later who manages the sequence to uncensor when sufficient progress is made
 func _ready():
-    # probably put this in it's own file later? determine later
+    interactable.interacted.connect(_on_interact)
     interactable.update_desc("You're not allowed to see that")
+    uncensor(false)
 
 func uncensor(do_censor: bool = false):
     censor_bar.visible = do_censor
+    is_censored = false
     if do_censor: interactable.update_desc("You're not allowed to see that")
     else: interactable.update_desc("Are you proud of yourself?")
+
+func _on_interact(player: Player):
+    if is_censored:
+        player.hud.display_flavor_text(interactable.description)
+        return
+    # emit sequence here
+    player.hud.display_flavor_text(interactable.description)
+    print("Game ended wow.")
