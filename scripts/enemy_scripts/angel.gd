6705bad0afac467fdf3639c282fa6d8a83a87180
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2026-06-12 06:38:02 -0700

    change angel ai to teleport when losing track of player

--- scripts/enemy_scripts/angel.gd
+++ scripts/enemy_scripts/angel.gd
@@ -6,8 +6,6 @@ class_name Angel
 
 @onready var vision: EnemyVision = $VisionCone
 @onready var reset_timer = $ResetTimer
-@onready var idle_timer = $IdleTimer
-@onready var chase_timer = $ChaseTimer
 @onready var sprite: AnimatedSprite2D = $Body
 
 @onready var body = $Body
@@ -21,71 +19,45 @@ var scout_position: Vector2 = Vector2.ZERO
 
 func _ready() -> void:
 	super()
-
-	# collision_mask = PHYS_LAYERS.TERRAIN + PHYS_LAYERS.PLAYER
-	# collision_layer = PHYS_LAYERS.ENEMY
-
-	nav_agent.path_desired_distance = 100.0
-	nav_agent.target_desired_distance = 1.0
-	nav_agent.path_max_distance = 500.0
 	vision.body_in_view.connect(on_view)
 	vision.body_out_of_view.connect(_on_body_out_of_view)
-	chase_target = player
-	ai_state = State.IDLE
-	nav_agent.target_position = find_nearest_patrol_point()
-
-	# set_movement_target()
-
-
-
-	# flashlight.coverage_changed.connect(_on_flashlight_coverage_changed)
+	reset_timer.timeout.connect(_on_reset_timeout)
+	global_position = patrol_path.global_position
+	ai_state = State.PATROL
 
 func _physics_process(delta: float) -> void:
+	if is_stunned: return
 
 	if ai_state == State.CHASE:
+		nav_agent.target_desired_distance = 5
 		chase(delta)
 		try_damage_player()
 
-	if ai_state == State.IDLE:
-		move_to(delta, nav_agent.get_next_path_position())
-		if nav_agent.is_target_reached():
-			ai_state = State.PATROL
-			patrol_path.progress = patrol_path.get_parent().curve.get_closest_offset(to_local(global_position))
-
-	if ai_state == State.PATROL:
-		# print("Angel is patrolling.")
+	elif ai_state == State.PATROL:
 		patrol(delta)
-		# rotation = move_toward(rotation, 0, delta * turn_rate)
+		body.rotation = -global_rotation
 
+	elif ai_state == State.SCOUT:
+		nav_agent.target_desired_distance = 50
+		var reached = move_to(delta, scout_position)
+		if reached:
+			reset_timer.start()
+			ai_state = State.IDLE
 
-	# elif ai_state == State.SCOUT:
-	# 	nav_agent.target_desired_distance = 50
-	# 	var reached = move_to(delta, scout_position)
-
+	elif ai_state == State.IDLE:
+		if chase_target:
+			global_rotation = global_position.direction_to(chase_target.global_position).angle()
+			body.rotation = -global_rotation
 
 func alert_sound(alerter: Node2D) -> void:
 	ai_state = State.SCOUT
 	scout_position = alerter.global_position + alerter.global_position.direction_to(global_position) * 50 # offset from player by tiny bit
 	reset_timer.stop()
-	idle_timer.stop()
 
-func find_nearest_patrol_point() -> Vector2:
-	var nearest_point = patrol_path.get_parent().curve.get_closest_point(global_position)
-	return nearest_point
-
-
-func change_direction(direction:float) -> void:
-	if sign(direction) < 0:
-		body.flip_h = false
-	elif sign(direction) > 0:
-		body.flip_h = true
-
-func _on_flashlight_coverage_changed(is_active: bool, center: Vector2, radius: float, bounds: Rect2) -> void:
-	if is_active and bounds.has_point(global_position):
-		if global_position.distance_squared_to(center) <= radius * radius:
-			is_in_light = true
-	else:
-		is_in_light = false
+func alert_visual(alerter: Node2D) -> void:
+	super(alerter)
+	if alerter is not Player: return
+	on_view(alerter as Player)
 
 func trigger_stun(stun_time: float) -> void:
 	is_stunned = true
@@ -100,25 +72,26 @@ func turn_process(delta):
 	if direction > 0: sprite.flip_h = true
 	elif direction < 0: sprite.flip_h = false
 
-func on_view(player: Player) -> void:
-	if ai_state != State.CHASE:
-		ai_state = State.CHASE
-		# print("Angel spotted player, starting chase!")
-		chase_target = player
-	chase_timer.start()
+func on_view(seen_player: Player) -> void:
+	if ai_state == State.CHASE: return
+	ai_state = State.CHASE
+	chase_target = seen_player
+	reset_timer.stop()
 
-func _on_body_out_of_view(player: Player) -> void:
-	pass
-
-
-func _on_chase_timer_timeout() -> void:
-	chase_timer.stop()
-	# print("Angel lost sight of player, stopping chase.")
-	nav_agent.target_position = find_nearest_patrol_point()
+func _on_body_out_of_view() -> void:
+	if ai_state != State.CHASE: return
+	if reset_timer.time_left == 0: reset_timer.start()
 	ai_state = State.IDLE
+	# play screech noise here
 
+func _on_reset_timeout() -> void:
+	# play teleport sound effect
+	global_position = patrol_path.global_position
+	ai_state = State.PATROL
 
 func try_damage_player():
 	var collider = attack_ray.get_collider()
 	if collider is not Player: return
-	(collider as Player).attack(attack_damage, self)
+	if (collider as Player).attack(attack_damage, self):
+		# play sound effect of damaging player
+		pass
