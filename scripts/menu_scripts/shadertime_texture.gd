01f621dad9f6d418127179329299c161143b5083
nicky leung <nicky-leung@outlook.com>
2026-06-12 18:14:34 -0700

    fix game_over shaders

---
+++ scripts/menu_scripts/shadertime_texture.gd
@@ -0,0 +1,10 @@
+extends TextureRect
+
+func _ready():
+	if material:
+		material.set_shader_parameter("TIME", 0.0)
+
+func _process(delta):
+	if material:
+		var current_time = material.get_shader_parameter("TIME")
+		material.set_shader_parameter("TIME", current_time + delta)
