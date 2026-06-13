e01923e2b9ba503bddf056a9b0f131be20318860
Gurveer Raith <gurveerraith@gmail.com>
2026-06-12 22:29:53 -0700

    update weeping angel speed and dmg

--- scripts/room_scripts/room_managers/puzzle_room_2.gd
+++ scripts/room_scripts/room_managers/puzzle_room_2.gd
@@ -6,21 +6,21 @@ extends Node2D
 @onready var enemy_reset_timer: Timer = $EnemyResetTimer
 
 func _ready():
-    room_listener.player_entered.connect(enable_enemies)
-    room_listener.player_exited.connect(disable_enemies)
-    enemy_reset_timer.timeout.connect(reset_enemy_positions)
+	room_listener.player_entered.connect(enable_enemies)
+	room_listener.player_exited.connect(disable_enemies)
+	enemy_reset_timer.timeout.connect(reset_enemy_positions)
 
 func enable_enemies(player: Player):
-    for enemy in enemy_list:
-        enemy.chase_target = player
-        enemy.toggle_active(true)
-    enemy_reset_timer.stop()
+	for enemy in enemy_list:
+		enemy.chase_target = player
+		enemy.toggle_active(true)
+	enemy_reset_timer.stop()
 
 func disable_enemies(_player: Player):
-    for enemy in enemy_list:
-        enemy.toggle_active(false)
-    enemy_reset_timer.start()
+	for enemy in enemy_list:
+		enemy.toggle_active(false)
+	enemy_reset_timer.start()
 
 func reset_enemy_positions():
-    for enemy in enemy_list:
-        enemy.reset_rat()
+	for enemy in enemy_list:
+		enemy.reset_rat()
